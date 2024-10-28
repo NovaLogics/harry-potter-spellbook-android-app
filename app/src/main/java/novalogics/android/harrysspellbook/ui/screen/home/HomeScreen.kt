f8d0b1858963e05a58ee03e8f93596315ff6351c
NovaLogics <rajitha.shavinda@gmail.com>
2024-10-28 18:38:02 +0530

    feat [ ui ] : Add Home screen title and update shapes

--- app/src/main/java/novalogics/android/harrysspellbook/ui/screen/home/HomeScreen.kt
+++ app/src/main/java/novalogics/android/harrysspellbook/ui/screen/home/HomeScreen.kt
@@ -14,20 +14,25 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.MaterialTheme.colorScheme
+import androidx.compose.material3.MaterialTheme.typography
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -36,6 +41,7 @@ import coil.decode.GifDecoder
 import coil.request.ImageRequest
 import coil.size.Size
 import novalogics.android.harrysspellbook.R
+import novalogics.android.harrysspellbook.ui.common.component.StyledText
 import novalogics.android.harrysspellbook.ui.theme.SpellBookTheme
 import novalogics.android.harrysspellbook.util.Constants
 
@@ -59,6 +65,7 @@ fun ScreenUiContent(
     uiState : HomeUiState
 ){
     Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier
             .fillMaxSize()
             .background(colorScheme.surface)
@@ -77,6 +84,19 @@ fun ScreenUiContent(
             ),
             shape = MaterialTheme.shapes.medium
         ) {}
+
+        StyledText(
+            stringResId = R.string.app_name,
+            letterSpacing = R.dimen.latter_space_small_2dp,
+            style = typography.displayLarge,
+            fontSize = R.dimen.text_size_xlarge_24sp,
+            color = colorScheme.secondary,
+            modifier = Modifier
+                .padding(
+                    all = dimensionResource(id = R.dimen.padding_medium_16dp),
+                )
+
+        )
 
         GifCard()
 
@@ -116,8 +136,12 @@ fun GifCard() {
     ElevatedCard(
         modifier = Modifier
             .fillMaxWidth()
+            .padding(dimensionResource(id = R.dimen.padding_regular_8dp))
             .size(200.dp),
-        colors = CardDefaults.cardColors(Color.Black)
+        colors = CardDefaults.cardColors(Color.Black),
+        shape = MaterialTheme.shapes.small.copy(all = CornerSize(
+            dimensionResource(id = R.dimen.corner_radius_medium_8dp))
+        ),
     ) {
         val painter = rememberAsyncImagePainter(
             ImageRequest.Builder(LocalContext.current)
@@ -128,7 +152,9 @@ fun GifCard() {
         Image(
             painter = painter,
             contentDescription = "Animated GIF",
-            modifier = Modifier.fillMaxWidth().alpha(0.3F),
+            modifier = Modifier
+                .fillMaxWidth()
+                .alpha(0.3F),
             contentScale = ContentScale.FillWidth
         )
     }
