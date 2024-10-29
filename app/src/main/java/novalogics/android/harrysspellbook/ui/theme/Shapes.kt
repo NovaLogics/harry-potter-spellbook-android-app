969335462031fd18ec7704fdae7466ad1e2bb282
NovaLogics <rajitha.shavinda@gmail.com>
2024-10-29 21:34:28 +0530

    feat [ ui ] : Update App shapes

--- app/src/main/java/novalogics/android/harrysspellbook/ui/theme/Shapes.kt
+++ app/src/main/java/novalogics/android/harrysspellbook/ui/theme/Shapes.kt
@@ -5,12 +5,20 @@ import androidx.compose.material3.Shapes
 import androidx.compose.ui.unit.dp
 
 val Shapes = Shapes(
-    small = RoundedCornerShape(size = 2.dp),
-
+    small = RoundedCornerShape(
+        size = 4.dp
+    ),
     medium = RoundedCornerShape(
         topStart = 0.dp,
         topEnd = 0.dp,
         bottomStart = 36.dp,
         bottomEnd = 36.dp
-    )
+    ),
+    large = RoundedCornerShape(
+        topStart = 0.dp,
+        topEnd = 8.dp,
+        bottomStart = 8.dp,
+        bottomEnd = 0.dp
+    ),
+
 )
