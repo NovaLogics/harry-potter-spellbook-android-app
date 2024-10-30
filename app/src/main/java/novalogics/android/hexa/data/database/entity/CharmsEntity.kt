0dbb09287e38d72f4f042001a196297e5397f6ef
NovaLogics <rajitha.shavinda@gmail.com>
2024-10-30 22:14:06 +0530

    feat [data]: Update Entity structure

--- app/src/main/java/novalogics/android/hexa/data/database/entity/CharmsEntity.kt
+++ app/src/main/java/novalogics/android/hexa/data/database/entity/CharmsEntity.kt
@@ -3,13 +3,16 @@ package novalogics.android.hexa.data.database.entity
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.google.gson.annotations.SerializedName
 
 @Entity(tableName = "charms")
 data class CharmsEntity(
     @PrimaryKey
     @ColumnInfo(name = "spell_id")
+    @SerializedName("spell_id")
     val spellId: String,
 
+    @SerializedName("spell_name")
     @ColumnInfo(name = "spell_name")
     val spellName: String,
 
@@ -20,6 +23,7 @@ data class CharmsEntity(
     val type: String,
 
     @ColumnInfo(name = "light_color")
+    @SerializedName("light_color")
     val lightColor: String,
 
     @ColumnInfo(name = "pronunciation")
@@ -38,5 +42,6 @@ data class CharmsEntity(
     val section: String,
 
     @ColumnInfo(name = "is_section")
+    @SerializedName("is_section")
     val isSection: Boolean = false
 )
