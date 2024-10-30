b824abb1cae0b5f214a3e8d7a175b84cc62c8a4c
NovaLogics <rajitha.shavinda@gmail.com>
2024-10-30 18:49:45 +0530

    feat [data]: RoomDB-Update entity

--- app/src/main/java/novalogics/android/hexa/data/database/entity/CharmsEntity.kt
+++ app/src/main/java/novalogics/android/hexa/data/database/entity/CharmsEntity.kt
@@ -1,21 +1,42 @@
 package novalogics.android.hexa.data.database.entity
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import com.google.gson.annotations.SerializedName
 
 @Entity(tableName = "charms")
 data class CharmsEntity(
     @PrimaryKey
-    val spellId: String = String(),
-    val spellName: String  = String(),
-    val description: String = String(),
-    val type: String = String(),
-    val lightColor: String = String(),
-    val pronunciation: String = String(),
-    val effect: String = String(),
-    val origin: String = String(),
-    val notes: String = String(),
-    val section: String = String(),
+    @ColumnInfo(name = "spell_id")
+    val spellId: String,
+
+    @ColumnInfo(name = "spell_name")
+    val spellName: String,
+
+    @ColumnInfo(name = "description")
+    val description: String,
+
+    @ColumnInfo(name = "type")
+    val type: String,
+
+    @ColumnInfo(name = "light_color")
+    val lightColor: String,
+
+    @ColumnInfo(name = "pronunciation")
+    val pronunciation: String,
+
+    @ColumnInfo(name = "effect")
+    val effect: String,
+
+    @ColumnInfo(name = "origin")
+    val origin: String? = null,
+
+    @ColumnInfo(name = "notes")
+    val notes: String? = null,
+
+    @ColumnInfo(name = "section")
+    val section: String,
+
+    @ColumnInfo(name = "is_section")
     val isSection: Boolean = false
 )
