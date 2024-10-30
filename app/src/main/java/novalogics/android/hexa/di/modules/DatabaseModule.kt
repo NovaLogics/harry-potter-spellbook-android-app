1055b7fb50e2412721c0750188ebdf4942bbbc13
NovaLogics <rajitha.shavinda@gmail.com>
2024-10-30 18:58:28 +0530

    feat [data]: RoomDB-Complete DatabaseModule

--- app/src/main/java/novalogics/android/hexa/di/modules/DatabaseModule.kt
+++ app/src/main/java/novalogics/android/hexa/di/modules/DatabaseModule.kt
@@ -1,10 +1,32 @@
 package novalogics.android.hexa.di.modules
 
+import android.content.Context
+import androidx.room.Room
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import novalogics.android.hexa.data.database.HexaDatabase
+import novalogics.android.hexa.data.database.dao.CharmsDao
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object DatabaseModule {
+
+    @Singleton
+    @Provides
+    fun provideDatabase(@ApplicationContext context: Context): HexaDatabase {
+        return Room.databaseBuilder(
+            context,
+            HexaDatabase::class.java,
+            "hexa"
+        ).build()
+    }
+
+    @Provides
+    fun provideYourDao(database: HexaDatabase): CharmsDao {
+        return database.charmsDao()
+    }
 }
