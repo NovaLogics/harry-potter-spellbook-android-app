9f617bccd0b0492b68081c6c2e9e17eb43d61beb
NovaLogics <rajitha.shavinda@gmail.com>
2024-10-30 22:13:43 +0530

    feat [data]: Update DataStore key structure

--- app/src/main/java/novalogics/android/hexa/data/pref/DataStoreKey.kt
+++ app/src/main/java/novalogics/android/hexa/data/pref/DataStoreKey.kt
@@ -1,9 +1,13 @@
 package novalogics.android.hexa.data.pref
 
 import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.booleanPreferencesKey
 import androidx.datastore.preferences.core.stringPreferencesKey
 
-sealed class DataStoreKey(val key: Preferences.Key<String>) {
-    data object UpgradeToRoom : DataStoreKey(stringPreferencesKey("upgrade_to_room"))
-    data object Username : DataStoreKey(stringPreferencesKey("username"))
+sealed class DataStoreStringKey(val key: Preferences.Key<String>) {
+    data object Username : DataStoreStringKey(stringPreferencesKey("username"))
+}
+
+sealed class DataStoreBooleanKey(val key: Preferences.Key<Boolean>) {
+    data object UpgradeToRoom : DataStoreBooleanKey(booleanPreferencesKey("upgrade_to_room"))
 }
