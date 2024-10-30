6db7a85b2033e0cb69c11c396591cda1cda026eb
NovaLogics <rajitha.shavinda@gmail.com>
2024-10-30 20:33:36 +0530

    feat [data]: Migrate data Spells to Charms - viewModel 1

--- app/src/main/java/novalogics/android/hexa/ui/screen/charms/CharmsUiState.kt
+++ app/src/main/java/novalogics/android/hexa/ui/screen/charms/CharmsUiState.kt
@@ -1,9 +1,9 @@
 package novalogics.android.hexa.ui.screen.charms
 
-import novalogics.android.hexa.data.model.Spell
+import novalogics.android.hexa.data.database.entity.CharmsEntity
 
 data class CharmsUiState(
     val isLoading: Boolean = false,
-    val spellList: List<Spell> = emptyList(),
+    val spellList: List<CharmsEntity> = emptyList(),
     val error: String? = null
 )
--- app/src/main/java/novalogics/android/hexa/ui/screen/charms/CharmsViewModel.kt
+++ app/src/main/java/novalogics/android/hexa/ui/screen/charms/CharmsViewModel.kt
@@ -2,6 +2,7 @@ package novalogics.android.hexa.ui.screen.charms
 
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
@@ -13,12 +14,14 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import novalogics.android.hexa.data.database.dao.CharmsDao
 import novalogics.android.hexa.data.database.entity.CharmsEntity
+import novalogics.android.hexa.data.pref.DataStoreManager
 import novalogics.android.hexa.data.repository.LocalDataSource
 import novalogics.android.hexa.util.Constants.DELAY_2_SECONDS
 import javax.inject.Inject
 
 @HiltViewModel
 class CharmsViewModel @Inject constructor(
+    private val dataStoreManager: DataStoreManager,
     private val charmsDao: CharmsDao,
     private val localRepository: LocalDataSource
 ) : ViewModel() {
@@ -27,6 +30,7 @@ class CharmsViewModel @Inject constructor(
     val uiState: StateFlow<CharmsUiState> = _uiState
 
     val allData = mutableStateOf<List<CharmsEntity>>(emptyList())
+    val upgradeToRoomState = dataStoreManager.getJsonToRoomUpgradeState().asLiveData()
 
     init {
         loadDataOffline()
@@ -37,13 +41,18 @@ class CharmsViewModel @Inject constructor(
             ui.copy(isLoading = true)
         }
 
+
+
         viewModelScope.launch {
             // Load data on a background thread
             val data = withContext(Dispatchers.IO) {
                 localRepository.getListOfSpells()
             }
 
-            delay(DELAY_2_SECONDS)
+            if(upgradeToRoomState.value == false){
+                charmsDao.getAllData()
+            }
+
 
             // Update UI state with data and set isLoading to false
             _uiState.update { ui ->
