7ad5d0d7874d5993b6ec4e6d015b58ebf3931d25
NovaLogics <rajitha.shavinda@gmail.com>
2024-10-30 19:03:50 +0530

    feat [data]: RoomDB-Add DAO to viewModels

--- app/src/main/java/novalogics/android/hexa/ui/screen/charms/CharmsViewModel.kt
+++ app/src/main/java/novalogics/android/hexa/ui/screen/charms/CharmsViewModel.kt
@@ -1,5 +1,6 @@
 package novalogics.android.hexa.ui.screen.charms
 
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -10,17 +11,22 @@ import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import novalogics.android.hexa.data.database.dao.CharmsDao
+import novalogics.android.hexa.data.database.entity.CharmsEntity
 import novalogics.android.hexa.data.repository.LocalDataSource
 import novalogics.android.hexa.util.Constants.DELAY_2_SECONDS
 import javax.inject.Inject
 
 @HiltViewModel
 class CharmsViewModel @Inject constructor(
+    private val charmsDao: CharmsDao,
     private val localRepository: LocalDataSource
 ) : ViewModel() {
 
     private val _uiState = MutableStateFlow(CharmsUiState())
     val uiState: StateFlow<CharmsUiState> = _uiState
+
+    val allData = mutableStateOf<List<CharmsEntity>>(emptyList())
 
     init {
         loadDataOffline()
