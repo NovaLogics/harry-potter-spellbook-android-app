550fff2e1847d04ff940357bd869b194599b8138
NovaLogics <rajitha.shavinda@gmail.com>
2024-10-30 18:56:43 +0530

    feat [data]: RoomDB-Add DatabaseModule

---
+++ app/src/main/java/novalogics/android/hexa/di/modules/DatabaseModule.kt
@@ -0,0 +1,10 @@
+package novalogics.android.hexa.di.modules
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DatabaseModule {
+}
