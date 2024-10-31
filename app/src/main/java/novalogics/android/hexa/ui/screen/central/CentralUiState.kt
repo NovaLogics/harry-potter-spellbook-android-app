fd32451644efe6f843ffc56b4bbbe899ca378f0a
NovaLogics <rajitha.shavinda@gmail.com>
2024-10-31 20:26:26 +0530

    feat [ui]: Update welcome message

--- app/src/main/java/novalogics/android/hexa/ui/screen/central/CentralUiState.kt
+++ app/src/main/java/novalogics/android/hexa/ui/screen/central/CentralUiState.kt
@@ -4,9 +4,9 @@ import novalogics.android.hexa.ui.util.aiEngine.HexaActions
 
 data class CentralUiState(
     val isLoading: Boolean = false,
-    val dataAiValue: String = "Welcome to app, how is your day",
+    val dataAiValue: String = "✨ Welcome to Hexa! Your magical companion awaits. ✨",
     val actionGo: HexaActions = HexaActions.NONE,
-    val listData: String = "-",
+    val listData: String = "",
     val textFieldValue: String = "",
     val error: String? = null
 )
