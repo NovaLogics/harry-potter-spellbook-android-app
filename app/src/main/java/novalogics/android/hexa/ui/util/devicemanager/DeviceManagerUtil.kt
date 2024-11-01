1f1e1882aa25b7b7ff00cdd6dc8f02134c7ed6d5
NovaLogics <rajitha.shavinda@gmail.com>
2024-11-01 16:18:58 +0530

    feat [util]: Implement get Flashlight Status fun

--- app/src/main/java/novalogics/android/hexa/ui/util/devicemanager/DeviceManagerUtil.kt
+++ app/src/main/java/novalogics/android/hexa/ui/util/devicemanager/DeviceManagerUtil.kt
@@ -4,14 +4,19 @@ import android.content.Context
 import android.hardware.camera2.CameraAccessException
 import android.hardware.camera2.CameraManager
 import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
 import javax.inject.Inject
 
 class DeviceManagerUtil @Inject constructor(
     @ApplicationContext private val context: Context
 ) : DeviceManagerUtilImpl {
 
+    private val cameraManager = context.getSystemService(Context.CAMERA_SERVICE) as CameraManager
+
     override fun deviceFlashlight(actionOnOff: Boolean) {
-        val cameraManager = context.getSystemService(Context.CAMERA_SERVICE) as CameraManager
+
         val cameraId = cameraManager.cameraIdList.first()
         try {
             cameraManager.setTorchMode(cameraId, actionOnOff)
@@ -19,4 +24,27 @@ class DeviceManagerUtil @Inject constructor(
             e.printStackTrace()
         }
     }
+
+    override fun getFlashlightStatus(): Flow<Boolean> = callbackFlow {
+        cameraManager.cameraIdList.firstOrNull() ?: run {
+            close()  // If no camera is found, close the flow
+            return@callbackFlow
+        }
+
+        // TorchCallback to listen for flashlight state changes
+        val torchCallback = object : CameraManager.TorchCallback() {
+            override fun onTorchModeChanged(cameraId: String, enabled: Boolean) {
+                trySend(enabled)
+            }
+        }
+
+        // Register the callback
+        cameraManager.registerTorchCallback(torchCallback, null)
+
+        // Clean up callback when the flow is closed
+        awaitClose {
+            cameraManager.unregisterTorchCallback(torchCallback)
+        }
+    }
+
 }
--- app/src/main/java/novalogics/android/hexa/ui/util/devicemanager/DeviceManagerUtilImpl.kt
+++ app/src/main/java/novalogics/android/hexa/ui/util/devicemanager/DeviceManagerUtilImpl.kt
@@ -1,6 +1,10 @@
 package novalogics.android.hexa.ui.util.devicemanager
 
+import kotlinx.coroutines.flow.Flow
+
 interface DeviceManagerUtilImpl  {
 
     fun deviceFlashlight( actionOnOff: Boolean )
+
+    fun getFlashlightStatus() : Flow<Boolean>
 }
