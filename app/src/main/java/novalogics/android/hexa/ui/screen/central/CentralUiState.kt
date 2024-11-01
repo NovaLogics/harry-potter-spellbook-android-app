b66db99fea22078d68a56b6c75aae9d893f17818
NovaLogics <rajitha.shavinda@gmail.com>
2024-11-01 18:23:49 +0530

    feat [util]: Update initial ui data structure

--- app/src/main/java/novalogics/android/hexa/ui/screen/central/CentralUiState.kt
+++ app/src/main/java/novalogics/android/hexa/ui/screen/central/CentralUiState.kt
@@ -4,10 +4,10 @@ import novalogics.android.hexa.ui.util.aiEngine.HexaActions
 
 data class CentralUiState(
     val isLoading: Boolean = false,
-    val dataAiValue: String = "✨ Welcome to Hexa! Your magical companion awaits. ✨",
+    val dataAiValue: String = "✨ Welcome to Hexa! \nYour magical companion awaits. ✨",
     val deviceHexaActions: HexaActions = HexaActions.NONE,
     val deviceStatus: String = "",
-    val userMessage: String = "",
+    val userMessage: String = "✨ \uD83E\uDD89 ✨",
     val textFieldValue: String = "",
     val error: String? = null
 )
